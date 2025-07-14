c9373ff0e63e7a134505c23ff21dffd6be87dbb0
Bashamega <adambashaahmednaji@gmail.com>
2025-07-14 15:30:16 +0300

    Feat: prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// prettier.config.js
+export default {
+    printWidth: 100,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    arrowParens: 'always'
+};
