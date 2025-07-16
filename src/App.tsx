2c507f615bdd4a1b547ffb38f18bc52b5ef4950c
Bashamega <adambashaahmednaji@gmail.com>
2025-07-16 16:20:04 +0300

    Remove editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
-import { Editors } from './components/Editors';
 
 function App() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 dark:bg-gray-900 px-4">
-      <Editors />
-    </main>
+=    </main>
   );
 }
 
