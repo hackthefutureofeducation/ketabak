02e18fcbdda469313fa37b21bf1a6b05781074dd
Bashamega <adambashaahmednaji@gmail.com>
2025-07-16 16:21:35 +0300

    -

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 dark:bg-gray-900 px-4">
-=    </main>
+   </main>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { DarkModeProvider } from './DarkModeProvider';
+import { DarkModeProvider } from './providers/DarkModeProvider';
 import NavBar from './components/navBar';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
