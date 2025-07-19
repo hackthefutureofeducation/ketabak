639f37adaeff1be47b7bfc6b7b5611f71de80d2d
Bashamega <adambashaahmednaji@gmail.com>
2025-07-19 09:53:19 +0300

    feat: Integrate Start component and conditional rendering in App for file selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css';
+import { Editors } from './components/Editors';
+import { Start } from './components/start';
+import { useFile } from './providers/FileProvider';
 
 function App() {
+  const { fileUrl } = useFile();
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 dark:bg-gray-900 px-4">
-   </main>
+      {fileUrl ? <Editors /> : <Start />}
+    </main>
   );
 }
 
