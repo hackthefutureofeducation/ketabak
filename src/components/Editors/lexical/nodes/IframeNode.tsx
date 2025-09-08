305aa683efc4d07a4fdf04c19f438a4430b68356
Bashamega <adambashaahmednaji@gmail.com>
2025-09-08 14:01:32 +0300

    -

--- src/components/Editors/lexical/nodes/IframeNode.tsx
+++ src/components/Editors/lexical/nodes/IframeNode.tsx
@@ -4,6 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
+ * Modified by Adam Naji on 2025-09-08 to suit project requirements.
  */
 
 import type {
