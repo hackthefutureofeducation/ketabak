9f566fdd8438ce6b4d2e23a1ca403918fa341126
Bashamega <adambashaahmednaji@gmail.com>
2026-03-16 11:31:12 +0200

    feat: add command-line argument support to bump.py for version updates in Cargo.toml

--- scripts/bump.py
+++ scripts/bump.py
@@ -1,5 +1,19 @@
-import toml; 
-f = 'src-tauri/Cargo.toml'
-data = toml.load(f)
-data['package']['version'] = '$version'
-with open(f, 'w') as out: toml.dump(data, out)
+import toml
+import sys
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser(description='Bump the version in Cargo.toml')
+    parser.add_argument('--version', required=True, help='Version to set')
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    f = 'src-tauri/Cargo.toml'
+    data = toml.load(f)
+    data['package']['version'] = args.version
+    with open(f, 'w') as out:
+        toml.dump(data, out)
+
+if __name__ == '__main__':
+    main()
