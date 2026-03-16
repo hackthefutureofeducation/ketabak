c6777cd4278bc70850a28666dd6450dc1ca552de
Bashamega <adambashaahmednaji@gmail.com>
2026-03-16 10:00:20 +0200

    feat: add CreateNewPageWelcome component and integrate into EditorLayout
    - Introduced CreateNewPageWelcome component for creating a new page.
    - Updated EditorLayout to conditionally render CreateNewPageWelcome when no pages exist.

--- src/components/Editors/layout.tsx
+++ src/components/Editors/layout.tsx
@@ -1,14 +1,27 @@
 import { EpubManagerProvider } from '../../providers/EpubManager';
 import { Editors } from './editors';
 import { Sidebar } from './sidebar';
+import { CreateNewPageWelcome } from './createNewPage';
+import { useEpubManager } from '../../providers/EpubManager';
+
+// LayoutInner is an internal layout for displaying pages/sidebar or create new page prompt
+function LayoutInner() {
+  const { pages } = useEpubManager();
+  const isNoPages = pages.length === 0;
+  if (isNoPages) return <CreateNewPageWelcome/>
+
+  return (
+    <section className="w-full grid grid-cols-[250px_1fr]">
+      <Sidebar />
+        <Editors/>
+    </section>
+  );
+}
 
 export function EditorLayout() {
   return (
     <EpubManagerProvider>
-      <section className="w-full grid grid-cols-[250px_1fr]">
-        <Sidebar />
-        <Editors />
-      </section>
+      <LayoutInner />
     </EpubManagerProvider>
   );
 }
